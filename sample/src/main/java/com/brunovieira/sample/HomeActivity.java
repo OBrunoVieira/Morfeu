9810eb26aba6e794abbdb5fc069125db931de6be
bruno.vieira <bruno.vieira@concretesolutions.com.br>
2016-05-21 14:54:49 -0300

    added sample config

--- sample/src/main/java/com/brunovieira/sample/HomeActivity.java
+++ sample/src/main/java/com/brunovieira/sample/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.brunovieira.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.brunovieira.morfeu.Morfeu;
 
